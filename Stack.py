ea0ed61e705b269c57179140efd2eadad745de7b
Akh2011 <simpiakshay2011@gmail.com>
2023-12-27 21:58:17 +0530

    Adding tree

--- Stack.py
+++ Stack.py
@@ -16,5 +16,13 @@ print (' After pop operating ')
 for i in s:
     print(i) 
 
+q =[]
+q.insert(0, 'https : //www.cnn.com')
+q.insert(0, 'https : //www.cnn.com/world')
+q.insert(0, 'https : //www.cnn.com/india')
+q.insert(0, 'https : //www.cnn.com/china')
 
+q.pop()
 
+for i in q:
+    print (i)
